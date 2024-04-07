e91a350afa5e1231570ac5bd99731b58641d0dca
Maria Chiara Bocchi <95356781+meryyr@users.noreply.github.com>
2024-04-07 20:55:42 +0200

    Update long_read_seq_analysis.R

--- long_read_seq_analysis.R
+++ long_read_seq_analysis.R
@@ -1,9 +1,9 @@
 library("Biostrings")
 library(seqRFLP)     #to tranform a dataframe in FASTA file
 
-fasta <- readDNAStringSet("SRR23409825_1.fasta")  #FASTA file
-head(fasta)
-fasta.df <- data.frame(as.character(fasta))  #transform the FASTA file in a dataframe to handle data better
+fasta <- readDNAStringSet("PacBio_sample1.fasta")  #FASTA file of PacBio long reads
+#head(fasta)
+fasta.df <- data.frame(as.character(fasta))        #transform the FASTA file in a data frame to handle data better
 
 
 fusions <- readDNAStringSet("short_reads_fusion.txt")   #file containing the fusion transcripts identified by short reads
@@ -12,20 +12,18 @@ sequence = paste(fusions)
 df.fusions <- data.frame(seq_name, sequence)            #transform the fusion transcripts data (short_reads_fusion.txt) in a dataframe
 
 
-#For each short_read_fusion, look 
+#For each short_read_fusion, find matches in long reads data (FASTA file)
 for (i in 1:length(sequence)){
-  print(i)
+  #print(i)                                            
   seq <- (sequence[i])
   print(sequence[i])
   fusion_name <- seq_name[i]
   match <- vmatchPattern(seq,fasta,max.mismatch=2)
-  #print(match)
   match.df <- data.frame(as.character(unlist(match)))
   seq.matched <-subset(fasta.df,rownames(fasta.df) %in% rownames(match.df))
   if (nrow(seq.matched) != 0) {
     fasta.match<- dataframe2fas(seq.matched, file = paste(fusion_name,".fasta", sep =''))
   }
-  
 }
 
 
