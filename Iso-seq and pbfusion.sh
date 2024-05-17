bd2bb429c13f79ed0b6d96f0dcdc0664bf02627c
Maria Chiara Bocchi <95356781+meryyr@users.noreply.github.com>
2024-05-17 16:46:43 +0200

    Update Iso-seq and pbfusion.sh

--- Iso-seq and pbfusion.sh
+++ Iso-seq and pbfusion.sh
@@ -8,7 +8,7 @@
 #The following code was adapted from the original one: https://github.com/SziKayLeung/Whole_Transcriptome_Paper/tree/f3190b3d6edcbf9703f69acb58fa6f210968f5bb
 #The dataset used for this particular Iso-Seq and pbfusion pipeline was taken from this study: https://doi.org/10.1016/j.celrep.2021.110022
 #
-#ISO-SEQ WORKFLOW
+#ISO-SEQ and pbfusion WORKFLOW 
 #1. ccs--> produce HiFi reads from subreads. Use version SMRT LINK V7.0 (v7) or SMRT LINK V13.0 (v13) depending on the CHEMISTRY of the samples
   #Use ccs_v7 for samples with OLD CHEMISTRY (see IsoSeq_Processing/Human/All_Human_RawData_OldChem.txt from the GitHub mentioned above)
   #Use ccs_v13 for samples with NEW CHEMISTRY (see IsoSeq_Processing/Human/All_Human_RawData_NewChem.txt from the GitHub mentioned above)
@@ -17,12 +17,18 @@
   # <lima movie.ccs.bam barcoded_primers.fasta movieX.fl.bam --isoseq --dump-clips --dump-removed>
 #3. isoseq refine --> trimming of poly(A) tail and rapid concatemer identification and removal.
   # <isoseq refine movieX.5p--3p.fl.bam movieX.flnc.bam --require-polya>
-#4. isoseq cluster2 --> de novo isoform-level clustering scalable to large number of reads 
+#4. isoseq cluster2 --> FLNC reads are clustered by their sequencing similarity to produce isoform consensus sequences. This step is the last step of Iso-Seq analysis if no genome is provided
   # <isoseq cluster2 flnc.bam clustered.bam --singletons --log-file cluster2log.txt>
+#5. pbmm2 --> align the Iso-seq HiFi reads (flnc.bam file, full-length non-concatemer reads) and clustered.bam file
+  #
+#6. pbfusion gtf-cache
 
+#7. pbfusions discover
 
-export ccsv_7="/home/mbocchi/smrtlink/install/smrtlink-release_7.0.1.66975/bundles/smrttools/install/smrttools-release_7.0.1.66768/smrtcmds/bin/ccs"
+export ccs_v7="/home/mbocchi/smrtlink/install/smrtlink-release_7.0.1.66975/bundles/smrttools/install/smrttools-release_7.0.1.66768/smrtcmds/bin/ccs"
+export ccs_v13="/home/mbocchi/SMRT_ROOT/install/smrtlink-release_13.0.0.214433/smrtcmds/bin/ccs"
 export lima_v7="/home/mbocchi/smrtlink/install/smrtlink-release_7.0.1.66975/bundles/smrttools/install/smrttools-release_7.0.1.66768/smrtcmds/bin/lima"
+export lima_v13="/home/mbocchi/SMRT_ROOT/install/smrtlink-release_13.0.0.214433/smrtcmds/bin/lima"
 export isoseq3_v7="/home/mbocchi/smrtlink/install/smrtlink-release_7.0.1.66975/bundles/smrttools/install/smrttools-release_7.0.1.66768/smrtcmds/bin/isoseq3"
 export isoseq_v13="/home/mbocchi/SMRT_ROOT/install/smrtlink-release_13.0.0.214433/smrtcmds/bin/isoseq"
 
@@ -44,7 +50,7 @@ $isoseq_v13 cluster2 --version
 
 echo "1. Running ccs to get HiFi reads"
 $ccsv_7 --reportFile --minPasses=3 --minPredictedAccuracy=0.99 --logFile mylog.txt subreads.bam ccs.bam
-$ccsv_13 --reportFile --minPasses=3 --minPredictedAccuracy=0.99 --logFile mylog.txt subreads.bam ccs.bam
+$ccsv_13 subreads.bam ccs.bam
 echo "ccs finished"
 
 echo "2. Running lima"
@@ -53,6 +59,7 @@ echo "lima finished"
 
 echo "3. Running isoseq refine"
 $isoseq3_v7 refine fl.primer_5p--primer_3p.bam $FASTA flnc.bam --require-polya
+$isoseq_v13
 echo "isoseq refine finished"
 
 echo "4. Running isoseq cluster2"
@@ -63,14 +70,11 @@ echo "isoseq cluster2 finished"
 
 
 
-Version used : paper
 
 
 
 
 
-Isoform clustering
-FLNC reads are clustered by their sequencing similarity to produce isoform consensus sequences. This step is the last step of 
-Iso-Seq analysis if no genome is provided
+
 
 
