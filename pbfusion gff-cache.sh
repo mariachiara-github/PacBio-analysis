ffe64075791176a8edcd3457b87888b02b671409
Maria Chiara Bocchi <95356781+meryyr@users.noreply.github.com>
2024-05-17 17:20:53 +0200

    Update pbfusion gff-cache.sh

--- pbfusion gff-cache.sh
+++ pbfusion gff-cache.sh
@@ -6,19 +6,19 @@
 #SBATCH --time=5-12
 #SBATCH --export=ALL
 #
+#GENCODE (https://www.gencodegenes.org/human/release_38.html)
+#GTF file --> Comprehensive gene annotation, regions = ALL (https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/gencode.v38.chr_patch_hapl_scaff.annotation.gtf.gz)
+#FASTA file --> (https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/GRCh38.p13.genome.fa.gz)
 
 echo "1.pbfusion gff-cache version "
-
 pbfusion gff-cache --version 
 
 echo "2.Serializing the input gtf file"
-
 pbfusion gff-cache \
     --gtf gencode.v38.chr_patch_hapl_scaff.annotation.gtf \
     --gtf-out gencode.v38.chr_patch_hapl_scaff.annotation.gtf.bin
        
 echo "3.Compressing the annotation bin file"
-
 xz -c -9 --extreme gencode.v38.chr_patch_hapl_scaff.annotation.gtf.bin > gencode.v38.chr_patch_hapl_scaff.annotation.gtf.bin.xz
 #ls -oh gencode.v38.chr_patch_hapl_scaff.annotation.gtf.bin* | awk '{print $4, $NF}'
 
